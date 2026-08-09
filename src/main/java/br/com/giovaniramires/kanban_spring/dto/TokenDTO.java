71f4b1b0ec5d1cad44c1edfcaf2b977dff2714d5
Giovani Ramires <giovanicardoso1010@gmail.com>
2026-08-09 10:50:28 -0300

    feat: adiciona Usuario, UsuarioRepository e Dtos de autentificação

---
+++ src/main/java/br/com/giovaniramires/kanban_spring/dto/TokenDTO.java
@@ -0,0 +1,8 @@
+package br.com.giovaniramires.kanban_spring.dto;
+
+import lombok.Data;
+
+@Data
+public class TokenDTO {
+    private String token;
+}
--- src/main/java/br/com/giovaniramires/kanban_spring/model/Usuario.java
+++ src/main/java/br/com/giovaniramires/kanban_spring/model/Usuario.java
@@ -1,5 +1,53 @@
 package br.com.giovaniramires.kanban_spring.model;
 
-public class Usuario {
+import java.util.Collection;
+import java.util.List;
+
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+
+@Entity
+@Table(name = "usuarios")
+@Data
+public class Usuario implements UserDetails{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String senha;
+
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
     
 }
